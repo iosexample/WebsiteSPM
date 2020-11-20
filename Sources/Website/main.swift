b5e15ba1197c1dee290b7774a87bdc0b60d8ff84
donly <donly@magicalboy.com>
2020-11-20 15:04:04 +0800

    first commit

---
+++ Sources/Website/main.swift
@@ -0,0 +1,36 @@
+import NIO
+import WebsiteBuilder
+
+/// All idioms the app can respond with
+let idioms = [
+  "A blessing in disguise",
+  "Better late than never",
+  "Bite the bullet",
+  "Break a leg",
+  "Cutting corners",
+  "Get your act together",
+  "That's the last straw",
+  "You can say that again"
+]
+
+struct IdiomResponser: HTTPResponder {
+  func respond(to request: HTTPRequest) -> EventLoopFuture<HTTPResponse> {
+    guard let randomIdiom = idioms.randomElement() else {
+      return request.eventLoop.newFailedFuture(error: Error.noIdiomsAvailable)
+    }
+    
+    let response = HTTPResponse(status: .ok, body: HTTPBody(text: randomIdiom))
+    return request.eventLoop.newSucceededFuture(result: response)
+  }
+  
+  enum Error: Swift.Error {
+    /// This error occurs if the idiom list is empty
+        case noIdiomsAvailable
+  }
+}
+
+/// Creates a new website responding with the IdiomResponder
+let website = Website(responder: IdiomResponser())
+
+/// Runs the website at the default port
+try website.run()
